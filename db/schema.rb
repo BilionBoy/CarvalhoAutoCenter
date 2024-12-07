e9f6553006001335ab7d02d057771faa787e863b
BilionBoy <dhiogoandre26@gmail.com>
2024-12-06 20:56:18 -0400

    refactor: services

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_140027) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "produtos", force: :cascade do |t|
+    t.string "nome"
+    t.text "descricao"
+    t.decimal "preco"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "rodas", force: :cascade do |t|
     t.string "titulo"
     t.text "descricao"
