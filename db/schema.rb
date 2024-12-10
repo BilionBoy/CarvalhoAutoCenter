ec7a4ac83205ce8dabd98a30f3cd4802d25624e1
BilionBoy <dhiogoandre26@gmail.com>
2024-12-10 12:38:57 -0400

    feature: adicionar estoque roda

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_03_140027) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_10_151154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,12 +42,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_140027) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "produtos", force: :cascade do |t|
-    t.string "nome"
-    t.text "descricao"
-    t.decimal "preco"
+  create_table "pedidos", force: :cascade do |t|
+    t.bigint "roda_id", null: false
+    t.string "codigo"
+    t.string "status", default: "pendente"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["roda_id"], name: "index_pedidos_on_roda_id"
   end
 
   create_table "rodas", force: :cascade do |t|
@@ -58,6 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_140027) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "categoria"
+    t.integer "quantidade", default: 0
   end
 
   create_table "services", force: :cascade do |t|
@@ -98,4 +100,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_140027) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "pedidos", "rodas"
 end
