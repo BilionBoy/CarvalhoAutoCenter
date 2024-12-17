727103146ab072fc36bbae83374cab304282bca4
BilionBoy <dhiogoandre26@gmail.com>
2024-12-16 21:08:23 -0400

    feature: db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
   def services
     @q = Roda.ransack(params[:q])  # Inicializa a busca
     @rodas = @q.result(distinct: true)  # Executa a pesquisa, se houver
     @no_results = @rodas.empty?  # Verifica se não há resultados
   end
-  
 
   def up
     render json: { status: "UP", time: Time.now }, status: :ok
   end
-
 end
