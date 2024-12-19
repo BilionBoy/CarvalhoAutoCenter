f98e9dd7feb88a335d50d68da67dfe2dbb80577c
BilionBoy <dhiogoandre26@gmail.com>
2024-12-19 08:23:36 -0400

    adicionar quantidade rodas

--- db/migrate/20241214192142_add_quantidade_to_rodas.rb
+++ db/migrate/20241214192142_add_quantidade_to_rodas.rb
@@ -1,5 +1,7 @@
 class AddQuantidadeToRodas < ActiveRecord::Migration[7.1]
   def change
-    add_column :rodas, :quantidade, :integer, default: 0, null: false
+    unless column_exists?(:rodas, :quantidade)
+      add_column :rodas, :quantidade, :integer, default: 0, null: false
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_14_194638) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_14_192142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,12 +56,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_14_194638) do
   create_table "rodas", force: :cascade do |t|
     t.string "titulo"
     t.text "descricao"
-    t.decimal "preco"
-    t.string "imagem"
+    t.decimal "preco", precision: 10, scale: 2
+    t.string "categoria"
+    t.integer "quantidade", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "categoria"
-    t.integer "quantidade", default: 0, null: false
   end
 
   create_table "users", force: :cascade do |t|
